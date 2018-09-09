ded31f1d7d1da6d1138c0bf7f4ef3bc785192238
Christine Dodrill <me@christine.website>
2018-09-08 19:12:23 -0700

    Docker (#20)
    * add Dockerfile
    
    * cwa/cwagi: runtime information page
    
    * update

--- cwa/cwagi/src/main.rs
+++ cwa/cwagi/src/main.rs
@@ -3,15 +3,14 @@ extern crate libcwa;
 use libcwa::env;
 use libcwa::log;
 use libcwa::stdio;
-use std::io::{Read, Write};
+use libcwa::runtime;
+use std::io::Write;
 use std::string::String;
 
 fn main() {}
 
 #[no_mangle]
 pub extern "C" fn cwa_main() -> i32 {
-    log::info("booted");
-
     match friendly_main() {
         Ok(()) => 0,
         Err(e) => e as i32,
@@ -19,10 +18,10 @@ pub extern "C" fn cwa_main() -> i32 {
 }
 
 #[derive(Debug)]
-struct Context {
+struct Context<'a> {
     pub method: String,
     pub request_uri: String,
-    pub body: libcwa::Resource,
+    pub body: &'a libcwa::Resource,
 }
 
 #[derive(Debug)]
@@ -38,10 +37,10 @@ pub fn friendly_main() -> Result<(), i32> {
     let fin = stdio::inp();
     let mut fout = stdio::out();
 
-    let ctx = Context {
+    let ctx: Context = Context {
         method: method,
         request_uri: request_uri,
-        body: fin,
+        body: &fin,
     };
 
     let resp: Response = respond_to(ctx);
@@ -73,10 +72,31 @@ fn getenv(name: &str) -> Option<String> {
     Some(result)
 }
 
+fn runtime_info(_ctx: Context) -> Response {
+    let mut result = String::new();
+    result.push_str("Hello, I am served from Rust compiled to wasm32-unknown-unknown.\n");
+    result.push_str("I know the following about the environment I am running in:\n");
+
+    let minor: i32 = runtime::spec_major();
+    let major: i32 = runtime::spec_major();
+    let rt_name: String = runtime::name();
+
+    result.push_str(&format!("I am running in {}, which implements version {}.{} of the CommonWebAssembly API.\n", rt_name, major, minor));
+
+    result.push_str(&format!("I think the current time is {}\n", libcwa::time::now()));
+
+    result.push_str("\nHere is my source code: https://github.com/Xe/olin/blob/master/cwa/cwagi/src/main.rs");
+
+    Response {
+        status: 200,
+        body: result,
+    }
+}
+
 fn respond_to(ctx: Context) -> Response {
     match ctx.request_uri.as_str() {
         "/cadey" => Response { status: 200, body: "you are awesome!".to_owned() },
-        _        => Response { status: 404, body: "try /cadey".to_owned() }
+        _        => runtime_info(ctx),
     }
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/Xe/olin
 
 replace github.com/go-interpreter/wagon v0.0.0 => github.com/losfair/wagon v0.0.0-20180825141017-f8cb99b55a39
 
-require github.com/perlin-network/life v0.0.0-20180827140726-c39ba803f741
+require (
+	github.com/Xe/ln v0.1.2
+	github.com/google/uuid v1.0.0 // indirect
+	github.com/pborman/uuid v0.0.0-20180906182336-adf5a7427709
+	github.com/perlin-network/life v0.0.0-20180827140726-c39ba803f741
+)
