092d7773f0d052a9bb6933024b4878bd841523d0
Christine Dodrill <me@christine.website>
2018-09-08 21:19:15 -0700

    cwa/tests: link random test in as a module

--- cwa/tests/src/ns/random.rs
+++ cwa/tests/src/ns/random.rs
@@ -5,7 +5,7 @@ use libcwa::{log, random};
 pub extern "C" fn test() -> Result<(), i32> {
     log::info("running ns::random tests");
 
-    log::info(&format!("i31: {}, i63: {}", random::i31(), random::i63));
+    log::info(&format!("i31: {}, i63: {}", random::i31(), random::i63()));
 
     log::info("ns::random tests passed");
     Ok(())
