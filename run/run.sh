7c504f7aed53e80954ee810e3da43d51081e88ec
Christine Dodrill <me@christine.website>
2018-09-09 15:33:20 -0700

    16

--- run/run.sh
+++ run/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/cwa-cgi -addr ":$PORT" -pool-size 2 -max-pool-size 128 -main-func cwa_main /main.wasm inside:docker
+/cwa-cgi -addr ":$PORT" -pool-size 16 -max-pool-size 128 -main-func cwa_main /main.wasm inside:docker
