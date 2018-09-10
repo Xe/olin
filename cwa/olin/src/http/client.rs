593b12ef1d0be9ec9184f66996634f14e40b69cf
Connie Hilarides <conni_h@outlook.com>
2018-09-09 21:14:50 -0400

    Fix `transport` #28 (#32)
    Rearrange things for borrow purposes
    Additionally, support for arbitrary response sizes

---
+++ cwa/olin/src/http/client.rs
@@ -0,0 +1,103 @@
+extern crate http;
+extern crate httparse;
+extern crate std;
+
+use std::io::{self, ErrorKind, Read, Write};
+use std::string::String;
+use std::vec::Vec;
+
+pub fn transport<'a>(
+    req: http::Request<&'a mut Vec<u8>>,
+    resp_body: &'a mut Vec<u8>,
+) -> Result<(http::Response<()>), io::Error> {
+    let mut fout: ::Resource = ::Resource::open("https://").map_err(|e| {
+        ::log::error(&format!("http: couldn't open {:?}", e));
+        io::Error::new(ErrorKind::Other, ::err::Error::Unknown)
+    })?;
+
+    let req = serialize_req(req);
+
+    fout.write(req.as_slice()).map_err(|e| {
+        ::log::error(&format!("http: couldn't write: {:?}", e));
+        e
+    })?;
+
+    fout.flush().map_err(|e| {
+        ::log::error(&format!("http: couldn't flush: {:?}", e));
+        e
+    })?;
+
+    let mut resp_code: u16 = 200;
+    let mut response = http::Response::builder();
+
+    let mut resp_data: Vec<u8> = Vec::new();
+    resp_data.resize(4096, 0u8);
+
+    let mut data_pos = 0;
+    loop {
+        if data_pos == resp_data.len() {
+            let new_len = resp_data.len() * 2;
+            resp_data.resize(new_len, 0u8);
+        }
+
+        data_pos += fout.read(&mut resp_data[data_pos..]).map_err(|e| {
+            ::log::error(&format!("http: couldn't read: {:?}", e));
+            e
+        })?;
+
+        // These must be defined here because they contain pointers into resp_data
+        let mut headers = [httparse::EMPTY_HEADER; 64];
+        let mut resp = httparse::Response::new(&mut headers);
+
+        let res = resp.parse(&resp_data).map_err(|e| {
+            ::log::error(&format!("http: couldn't parse response: {:?}", e));
+            io::Error::new(ErrorKind::Other, e)
+        })?;
+
+        // No need to repeat any work until parsing is complete
+        if res.is_partial() {
+            continue;
+        }
+
+        match resp.code {
+            None => {}
+            Some(code) => resp_code = code,
+        }
+
+        for hdr in resp.headers {
+            if hdr.name != "" {
+                response.header(
+                    hdr.name,
+                    std::string::String::from_utf8_lossy(hdr.value).into_owned(),
+                );
+            }
+        }
+        break;
+    }
+
+    Ok(response.status(resp_code).body(()).unwrap())
+}
+
+fn serialize_req<'a>(mut req: http::Request<&'a mut std::vec::Vec<u8>>) -> Vec<u8> {
+    let mut output = String::new();
+    let mut body = req.body_mut().clone();
+    let method = req.method().as_str();
+    let path = req.uri().path();
+
+    output.push_str(method);
+    output.push_str(" ");
+    output.push_str(path);
+    output.push_str(" HTTP/1.1\r\n");
+
+    for (key, value) in req.headers().iter() {
+        output.push_str(&format!("{:?}: {:?}", key, value));
+    }
+
+    output.push_str("\n\n");
+
+    let mut output = output.into_bytes();
+
+    output.append(&mut body);
+
+    output
+}
