221b28e6c11cd40074ab8d857e0b989d12d3e6ff
Mec-iS <tunedconsulting@gmail.com>
2018-09-11 11:23:04 +0100

    How-to for Twiggy

--- cwa/cwagi/build.sh
+++ cwa/cwagi/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cargo +nightly install wasm-gc
+cargo +nightly install wasm-snip
 cargo +nightly -Z unstable-options build --target wasm32-unknown-unknown --out-dir . --release
 wasm-gc cwagi.wasm
 wasm-snip cwagi.wasm -o cwagi.wasm main
