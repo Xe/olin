c35df466ee579138ebfab9c541912b4fdfa3838d
Christine Dodrill <me@christine.website>
2018-09-13 16:43:57 -0700

    Dockerfile: build rust to webassembly and test in docker build

--- cwa/cwagi/build.sh
+++ cwa/cwagi/build.sh
@@ -5,4 +5,3 @@ cargo +nightly install wasm-snip
 cargo +nightly -Z unstable-options build --target wasm32-unknown-unknown --out-dir . --release
 wasm-gc cwagi.wasm
 wasm-snip cwagi.wasm -o cwagi.wasm main
-cp cwagi.wasm ../../cmd/cwa-cgi/testdata/test.wasm
--- cwa/tests/build.sh
+++ cwa/tests/build.sh
@@ -2,4 +2,3 @@
 
 cargo +nightly -Z unstable-options build --target wasm32-unknown-unknown --out-dir . --release
 wasm-gc cwa-tests.wasm
-cp cwa-tests.wasm ../../cmd/cwa/testdata/test.wasm
