e37006dc0e9dc74669af78eb3383ca0cded2391f
Christine Dodrill <me@christine.website>
2018-10-18 20:06:45 -0700

    cwa/olin/bin: log-env-message has no_std, it works, cool

--- cwa/olin/src/bin/log-env-message.rs
+++ cwa/olin/src/bin/log-env-message.rs
@@ -1,20 +1,19 @@
 #![no_main]
+#![no_std]
 
 extern crate olin;
 
-use olin::{env, log, panic};
+use olin::{env, log};
 
 #[no_mangle]
 pub extern "C" fn cwa_main() -> i32 {
-    panic::set_hook();
-
     match env::get("MESSAGE") {
         Err(env::Error::NotFound) => {
             log::error("MESSAGE doesn't exist");
             return 1;
         }
         Ok(val) => {
-            log::info(&format!("{}", val));
+            log::info(&val);
         }
         _ => return 2,
     }
