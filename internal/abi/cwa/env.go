7009477291180eced145e7b8564bdec8eec3f9a8
Christine Dodrill <me@christine.website>
2019-03-21 11:03:41 -0700

    internal/abi/cwa: speedhack

--- internal/abi/cwa/env.go
+++ internal/abi/cwa/env.go
@@ -3,8 +3,7 @@ package cwa
 func (p *Process) EnvGet(keyPtr, keyLen, valPtr, valLen uint32) (int32, error) {
 	//p.logger.Printf("getenv: 0x%x:%d, 0x%x:%d", keyPtr, keyLen, valPtr, valLen)
 
-	key := string(readMem(p.vm.Memory, keyPtr, keyLen))
-	val, ok := p.env[key]
+	val, ok := p.env[string(readMem(p.vm.Memory, keyPtr, keyLen))]
 	if !ok {
 		return 0, NotFoundError
 	}
