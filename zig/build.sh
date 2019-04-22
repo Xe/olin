d20c1b1eb3c12cd68e99813d4d3961c2fcbe792c
Christine Dodrill <me@christine.website>
2019-04-22 22:22:12 +0000

    zig: do a test with both optimized and unoptimized coi

--- zig/build.sh
+++ zig/build.sh
@@ -6,5 +6,7 @@ set -x
 zig build-exe -target wasm32-freestanding-none src/coi.zig
 zig build-exe -target wasm32-freestanding-none --release-fast src/shaman.zig
 zig build-exe -target wasm32-freestanding-none --release-fast src/triangle.zig
-cwa -vm-stats -test -write-mem coi.mem.wasm coi.wasm
+cwa -vm-stats -test coi.wasm
+zig build-exe -target wasm32-freestanding-none --release-fast src/coi.zig
+cwa -vm-stats -test coi.wasm
 cwa -vm-stats triangle.wasm
