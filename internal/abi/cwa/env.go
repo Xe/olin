ede47df0e6e5321b0646e724f56d7e0544363b40
Christine Dodrill <me@christine.website>
2019-04-22 20:59:28 +0000

    cleanup

--- internal/abi/cwa/env.go
+++ internal/abi/cwa/env.go
@@ -1,20 +1,16 @@
 package cwa
 
 func (p *Process) EnvGet(keyPtr, keyLen, valPtr, valLen uint32) (int32, error) {
-	key := string(readMem(p.vm.Memory, keyPtr, keyLen))
-	val, ok := p.env[key]
+	val, ok := p.env[string(readMem(p.vm.Memory, keyPtr, keyLen))]
 	if !ok {
 		return 0, NotFoundError
 	}
 
-	if len(val) == int(valLen) {
-		for i, by := range []byte(val) {
-			p.vm.Memory[valPtr+uint32(i)] = by
-		}
+	if len(val) < int(valLen) {
+		copy(p.vm.Memory[valPtr:], []byte(val))
 	}
 
 	//p.logger.Printf("mem: 0x%x-0x%x: %x", valPtr, valPtr+valLen, p.vm.Memory[valPtr:valPtr+valLen])
-	//p.logger.Printf("getenv: 0x%x %d (%q), 0x%x %d returns (%q) %d", keyPtr, keyLen, key, valPtr, valLen, val, len(val))
 
 	return int32(len(val)), nil
 }
