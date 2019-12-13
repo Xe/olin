6a965418fcfba929f66cbc6989fe0a2781df7ad0
Christine Dodrill <me@christine.website>
2019-12-13 04:33:16 +0000

    cmd/cwa: fix test script

--- cmd/cwa/test.sh
+++ cmd/cwa/test.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-go run main.go -vm-stats -test -main-func cwa_main ./testdata/test.wasm foo bar
+go run main.go -vm-stats -test ./testdata/test.wasm foo bar
 go run main.go -vm-stats -test -go ./testdata/go.wasm foo bar ||:
