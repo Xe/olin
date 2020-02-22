f9802eda2c69d8f7aa7c11cefc9f255d8172fca5
Christine Dodrill <me@christine.website>
2020-02-22 16:33:47 +0000

    zig build

--- zig/build.sh
+++ zig/build.sh
@@ -3,18 +3,26 @@
 set -e
 set -x
 
-zig build-lib -target wasm32-other-none --release-fast src/allyourargs.zig
-zig build-lib -target wasm32-other-none --release-fast src/allyourbase.zig
-zig build-lib -target wasm32-other-none --release-fast src/allyourlogs.zig
-zig build-lib -target wasm32-other-none --release-fast src/runtime_name.zig
-zig build-lib -target wasm32-other-none --release-fast src/cat.zig
-zig build-lib -target wasm32-other-none --release-fast src/httptest.zig
-zig build-lib -target wasm32-other-none --release-fast src/shaman.zig
-zig build-lib -target wasm32-other-none --release-fast src/cwagi.zig
-zig build-lib -target wasm32-other-none --release-fast src/triangle.zig
-zig build-lib -target wasm32-other-none --release-fast src/coi.zig
-zig build-lib -target wasm32-other-none --release-fast src/exit0.zig
-zig build-lib -target wasm32-other-none --release-fast src/exit1.zig
+ZIGFLAGS="-target wasm32-other-none --release-fast"
+env
+
+if [ -n "$HOME" -a "$HOME" = "/homeless-shelter" ]
+then
+    ZIGFLAGS="${ZIGFLAGS} --cache-dir $(mktemp -d)"
+fi
+
+zig build-lib $ZIGFLAGS src/allyourargs.zig
+zig build-lib $ZIGFLAGS src/allyourbase.zig
+zig build-lib $ZIGFLAGS src/allyourlogs.zig
+zig build-lib $ZIGFLAGS src/runtime_name.zig
+zig build-lib $ZIGFLAGS src/cat.zig
+zig build-lib $ZIGFLAGS src/httptest.zig
+zig build-lib $ZIGFLAGS src/shaman.zig
+zig build-lib $ZIGFLAGS src/cwagi.zig
+zig build-lib $ZIGFLAGS src/triangle.zig
+zig build-lib $ZIGFLAGS src/coi.zig
+zig build-lib $ZIGFLAGS src/exit0.zig
+zig build-lib $ZIGFLAGS src/exit1.zig
 
 rm *.h *.o
 
